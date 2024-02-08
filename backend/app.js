d2ef547bd4900af94323da8cc819a225c31902ce
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-02-07 18:32:49 -0800

    update abckend
    create user signup endpoint

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var roomsRouter = require('./routes/rooms');
 
 var app = express();
 
@@ -15,27 +17,36 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/rooms', roomsRouter);
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    next();
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,112 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {hash} = require('bcrypt');
+const {v4: generateId} = require('uuid');
+const {isValidText, isValidEmail} = require("../utils/validation");
+const {createJSONToken, isValidPassword} = require("../utils/auth");
+const fs = require("node:fs/promises");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('get users');
 });
+
+router.post('/signup', async (req, res, next) => {
+    const data = req.body;
+    let errors = {};
+
+    if (!isValidEmail(data.email)) {
+        errors.email = 'Invalid email.';
+    } else {
+        try {
+            const existingUser = await get(data.email);
+            if (existingUser) {
+                errors.email = 'Email exists already.';
+            }
+        } catch (error) {
+            console.log("error get user")
+        }
+    }
+
+    if (!isValidText(data.password, 6)) {
+        errors.password = 'Invalid password. Must be at least 6 characters long.';
+    }
+
+    if (Object.keys(errors).length > 0) {
+        return res.status(422).json({
+            message: 'User signup failed due to validation errors.',
+            errors,
+        });
+    }
+
+    try {
+        const createdUser = await add(data);
+        // const authToken = createJSONToken(createdUser.email);
+        res
+            .status(200)
+            .json({message: 'User created.', user: createdUser});
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    let user;
+    try {
+        user = await get(email);
+    } catch (error) {
+        return res.status(401).json({message: 'Authentication failed.'});
+    }
+
+    const pwIsValid = await isValidPassword(password, user.password);
+    if (!pwIsValid) {
+        return res.status(422).json({
+            message: 'Invalid credentials.',
+            errors: {credentials: 'Invalid email or password entered.'},
+        });
+    }
+
+    const token = createJSONToken(email);
+    res.json({token});
+});
+
+
+async function add(data) {
+    let storedData = await readData();
+    const userId = generateId();
+    const hashedPw = await hash(data.password, 12);
+    if (!storedData) {
+        storedData = [];
+    }
+    storedData.push({...data, password: hashedPw, id: userId, reservations: []});
+    await writeData(storedData);
+    return {id: userId, email: data.email};
+}
+
+async function get(email) {
+    const storedData = await readData();
+    if (!storedData || storedData.length === 0) {
+        throw new Error('Could not find any users.');
+    }
+
+    const user = storedData.users.find((ev) => ev.email === email);
+    if (!user) {
+        throw new Error('Could not find user for email ' + email);
+    }
+
+    return user;
+}
+
+async function readData() {
+    const data = await fs.readFile('databases/users.json', 'utf8');
+    return JSON.parse(data);
+}
+
+async function writeData(data) {
+    await fs.writeFile('databases/users.json', JSON.stringify(data));
+}
 
 module.exports = router;
