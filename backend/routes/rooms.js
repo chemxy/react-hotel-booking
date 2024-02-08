20a268a08e25b848f73a945a979ed5f1755eef93
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-02-07 18:45:41 -0800

    Update rooms.js
    create skeleton for rooms endpoint

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -2,8 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 /* GET rooms listing. */
-router.get('/', function (req, res, next) {
-    res.send('get rooms ');
+router.get('/all', function (req, res, next) {
+    res.send('get all rooms ');
 });
+
+router.get('/room', function (req, res, next) {
+    res.send('get room by id ');
+});
+
+router.post('/reserve', function (req, res, next) {
+    res.send('reserve a room ');
+});
+
 
 module.exports = router;
