42c3e162cf1a6eccb3ca247927fecbd5eeb64570
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-02-14 18:25:38 -0800

    Update file.js
    organize exports

--- backend/utils/file.js
+++ backend/utils/file.js
@@ -1,10 +1,13 @@
 const fs = require("node:fs/promises");
- export async function readData(path) {
+
+async function readData(path) {
     const data = await fs.readFile(path, 'utf8');
     return JSON.parse(data);
 }
 
- export async function writeData(path, data) {
+async function writeData(path, data) {
     await fs.writeFile(path, JSON.stringify(data));
 }
 
+exports.readData = readData;
+exports.writeData = writeData;
