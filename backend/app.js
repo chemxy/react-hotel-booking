ebb158a3b4915b9ea86d6951e013d042f48408c9
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-02-28 17:31:00 -0800

    add cors to backend

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const roomsRouter = require('./routes/rooms');
 const reservationsRouter = require('./routes/reservations');
+const cors = require("cors");
 
 
 const app = express();
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -35,6 +37,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
     next();
 });
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
