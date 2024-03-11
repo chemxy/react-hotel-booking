f7f524daf34745a92b3ea3c2406a335569acf480
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-03-11 15:28:23 -0700

    Update file.js

--- backend/utils/file.js
+++ backend/utils/file.js
@@ -5,9 +5,9 @@ const fs = require("node:fs/promises");
 //     return JSON.parse(data);
 // }
 
-async function writeData(path, data) {
-    await fs.writeFile(path, JSON.stringify(data));
-}
+// async function writeData(path, data) {
+//     await fs.writeFile(path, JSON.stringify(data));
+// }
 
 // exports.readData = readData;
-exports.writeData = writeData;
+// exports.writeData = writeData;
