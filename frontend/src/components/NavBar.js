82b1e361fd1c1b421fb709f3f90764b1f2ec5425
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-10-23 23:27:07 -0700

    remove tailwindcss; add bootstrap ui

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,6 @@
 export function NavBar() {
 
-    return <>
+    return <div>
         <nav className="flex sm:justify-center space-x-4">
             {[
                 ['Home', '/dashboard'],
@@ -12,12 +12,5 @@ export function NavBar() {
                    className="capitalize rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
             ))}
         </nav>
-        <div
-            className="flex rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-            <input id="username" name="username" type="text" placeholder="janesmith" autoComplete="username"
-                   className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 focus:rounded-lg placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-            />
-        </div>
-
-    </>
+    </div>
 }
--- frontend/src/components/SearchComponent.js
+++ frontend/src/components/SearchComponent.js
@@ -7,8 +7,7 @@ export function SearchComponent() {
     useEffect(() => {
         fetch("http://localhost:3201/rooms/all", {
             headers: {
-                'Content-Type': 'application/json',
-                "Access-Control-Allow-Origin": '*'
+                'Content-Type': 'application/json', "Access-Control-Allow-Origin": '*'
             }
         }).then(response => {
 
@@ -25,17 +24,75 @@ export function SearchComponent() {
 
 
     return (
-        <div className="text-3xl font-bold underline">
-            dsadsa
-            {rooms.map(room => {
-                return (
-                    <div>
+        <div className="container">
+            <ul className="nav nav-tabs">
+                <li className="nav-item">
+                    <a className="nav-link active text-capitalize" aria-current="page" href="#hotels">hotels</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link text-capitalize" aria-current="page" href="#flights">flights</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link text-capitalize" aria-current="page" href="#cars">cars</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link text-capitalize" aria-current="page" href="#railways">railways</a>
+                </li>
+            </ul>
+            <div className="border border-top-0 pt-5 p-3 rounded-4 rounded-top-0">
+                <div className="d-flex flex-row justify-content-center">
+                    <div className="mb-3 w-25 me-3">
+                        <div>
+                            <label className="form-label text-capitalize">location</label>
+                        </div>
+                        <div className="d-flex input-group">
+                        <span
+                            className="material-symbols-outlined input-group-text bg-transparent border-end-0 ">location_on</span>
+                            <input type="text" className="form-control border-start-0" placeholder="Location"/>
+                        </div>
+                    </div>
+                    <div className=" mb-3 w-25 me-3">
+                        <div>
+                            <label className="form-label text-capitalize">check-in</label>
+                        </div>
+                        <div className="d-flex input-group">
+                         <span
+                             className="material-symbols-outlined input-group-text bg-transparent border-end-0">date_range</span>
+                            <input type="date" className="form-control border-start-0" placeholder="Check-in date"/>
+                        </div>
+                    </div>
+                    <div className=" mb-3 w-25 me-3">
+                        <div>
+                            <label className="form-label text-capitalize">check-out</label>
+                        </div>
+                        <div className="d-flex input-group">
+                         <span
+                             className="material-symbols-outlined input-group-text bg-transparent border-end-0">date_range</span>
+                            <input type="date" className="form-control border-start-0" placeholder="Check-out date"/>
+                        </div>
+                    </div>
+                    <div className="input-group mb-3 w-25 me-3">
+                        <div>
+                            <label className="form-label text-capitalize">Person</label>
+                        </div>
+                        <div className="d-flex input-group">
+                        <span
+                            className="material-symbols-outlined input-group-text bg-transparent border-end-0">person</span>
+                            <input type="text" className="form-control border-start-0" placeholder="Person"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="">
+                {rooms.map(room => {
+                    return (<div>
                         <h1>{room.roomid}</h1>
                         <h2>{room.name}</h2>
-                        <img src={room.image}/>
-                    </div>
-                )
-            })}
+                        <img src={room.image} alt=""/>
+                    </div>)
+                })}
+            </div>
         </div>
     );
 }
